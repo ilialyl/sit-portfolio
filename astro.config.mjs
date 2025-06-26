f07bd099f71e397f944a000e2978d526984ecf1e
lyns0 <lynzies.sl@gmail.com>
2025-06-26 14:45:56 +0700

    Add tailwind to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://cyandizy.github.io",
   base: "sit-portfolio",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
