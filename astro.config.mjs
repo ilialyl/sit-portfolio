e8e0ce7ed39c1d90ec31953abab50db9f3089069
lyns0 <lynzies.sl@gmail.com>
2025-06-30 14:40:53 +0700

    Add contact section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://cyandizy.github.io",
@@ -11,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
