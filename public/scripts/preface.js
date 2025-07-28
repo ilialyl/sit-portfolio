1b5a356e3b977034e1dc52bed27478523158918f
lyns0 <lynzies.sl@gmail.com>
2025-07-28 18:00:21 +0700

    update toggleLanguage to target class instead of id

--- public/scripts/preface.js
+++ public/scripts/preface.js
@@ -1,7 +1,7 @@
-export function toggleLanguage(buttonId, jpId, enId) {
+export function toggleLanguage(buttonId, jpClass, enClass) {
   const button = document.getElementById(buttonId);
-  const japanese = document.getElementById(jpId);
-  const english = document.getElementById(enId);
+  const japanese = document.getElementsByClassName(jpClass);
+  const english = document.getElementsByClassName(enClass);
   const Language = {
     EN: 0,
     JP: 1,
@@ -9,19 +9,25 @@ export function toggleLanguage(buttonId, jpId, enId) {
 
   let currentLang = Language.EN;
 
-  if (
-    button instanceof HTMLElement &&
-    japanese instanceof HTMLElement &&
-    english instanceof HTMLElement
-  )
+  if (button instanceof HTMLElement)
     button.addEventListener("click", () => {
       if (currentLang == Language.EN) {
-        english.classList.add("hidden");
-        japanese.classList.remove("hidden");
+        for (const element of english) {
+          element.classList.add("hidden");
+        }
+        for (const element of japanese) {
+          element.classList.remove("hidden");
+        }
+        button.textContent = "English";
         currentLang = Language.JP;
       } else {
-        japanese.classList.add("hidden");
-        english.classList.remove("hidden");
+        for (const element of english) {
+          element.classList.remove("hidden");
+        }
+        for (const element of japanese) {
+          element.classList.add("hidden");
+        }
+        button.textContent = "日本語";
         currentLang = Language.EN;
       }
     });
