9c59b7dd758c97b91de24acef70cdda51e95a616
lyns0 <lynzies.sl@gmail.com>
2025-08-03 14:38:24 +0700

    update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cyandizy.github.io",
+  site: "https://ilialyl.github.io",
   base: "/sit-portfolio/",
 
   vite: {
